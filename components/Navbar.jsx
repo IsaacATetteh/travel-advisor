89803055890b744a2bc98381e00d793440735556
IsaacATetteh <iabbeytetteh@gmail.com>
2024-05-31 18:54:09 +0100

    Dynamic sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,11 +3,13 @@ import { React, useState } from "react";
 import Image from "next/image";
 import Logo from "../public/Logo.jpg";
 import Link from "next/link";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const handleNav = () => {};
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav className="fixed w-full h-24 shadow-xl bg-white ">
@@ -50,6 +52,55 @@ const Navbar = () => {
         </div>
         <div className="sm:hidden cursor-pointer pl-24" onClick={handleNav}>
           <AiOutlineMenu size={25} />
+        </div>
+      </div>
+      <div
+        className={
+          menuOpen
+            ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#d7d7d7] ease-in duration-500 p-10"
+            : "fixed left-[-100%] top-0 padding-10 ease-in duration-500"
+        }
+      >
+        <div className="w-full cursor-pointer flex items-center justify-end">
+          <div onClick={handleNav}>
+            <AiOutlineClose size={25} />
+          </div>
+        </div>
+        <div className="flex-col py-4">
+          <ul>
+            <Link href="/destination">
+              <li
+                className="py-4 uppercase hover:border-b text-xl"
+                onClick={handleNav}
+              >
+                Destination
+              </li>
+            </Link>
+            <Link href="/destination">
+              <li
+                className="py-4 uppercase hover:border-b text-xl"
+                onClick={handleNav}
+              >
+                About Us
+              </li>
+            </Link>
+            <Link href="/destination">
+              <li
+                className="py-4 uppercase hover:border-b text-xl"
+                onClick={handleNav}
+              >
+                Testomonial
+              </li>
+            </Link>
+            <Link href="/destination">
+              <li
+                className="py-4  uppercase hover:border-b text-xl"
+                onClick={handleNav}
+              >
+                Gallery
+              </li>
+            </Link>
+          </ul>
         </div>
       </div>
     </nav>
